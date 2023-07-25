272e99b8a72c827a14108d678c9bd3bcfd500df7
Peter Driscoll <patardriscoll@gmail.com>
2023-07-25 13:12:24 -0400

    testing secrets

--- flask-server/dto/Meal_Nutrient_Stats_DTO.py
+++ flask-server/dto/Meal_Nutrient_Stats_DTO.py
@@ -1,11 +1,12 @@
 from .Food_Nutrient_Stats_DTO import Food_Nutrient_Stats_DTO
+from dto.Extended_Meal_DTO import Extended_Meal_DTO
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from domain.Extended_Meal_Domain import Extended_Meal_Domain
-    from domain.Extended_Meal_Plan_Domain import Extended_Meal_Plan_Domain
+    from domain.Meal_Plan_Domain import Meal_Plan_Domain
     from .Nutrient_Daily_Value_DTO import Nutrient_Daily_Value_DTO
-    from dto.Extended_Meal_DTO import Extended_Meal_DTO
 
 
 class Meal_Nutrient_Stats_DTO(Food_Nutrient_Stats_DTO):
@@ -18,7 +19,7 @@ class Meal_Nutrient_Stats_DTO(Food_Nutrient_Stats_DTO):
         fat_k_cal: float,
         carb_k_cal: float,
         weight: float,
-        associated_meal_plan: "Extended_Meal_Plan_Domain",
+        associated_meal_plan: "Meal_Plan_Domain",
         associated_meal: "Extended_Meal_Domain",
     ):
         super().__init__(
@@ -31,4 +32,14 @@ class Meal_Nutrient_Stats_DTO(Food_Nutrient_Stats_DTO):
             weight=weight,
             associated_meal_plan=associated_meal_plan,
         )
-        self.associated_meal = Extended_Meal_DTO(meal_domain=associated_meal)
+        self.associated_meal = Extended_Meal_DTO(extended_meal_domain=associated_meal)
+
+    def serialize(self):
+        serialized_attributes = super().serialize()
+        serialized_nutrients = []
+        for nutrient in self.nutrients:
+            serialized_nutrient = nutrient.serialize()
+            serialized_nutrients.append(serialized_nutrient)
+        serialized_attributes["nutrients"] = serialized_nutrients
+
+        return serialized_attributes
