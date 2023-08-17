2723afecb68f7704c4c477d861cfdd5f41d92585
pvd232 <50126818+pvd232@users.noreply.github.com>
2023-08-17 14:42:12 -0400

    updated logging interface

--- flask-server/service/Logging_Service.py
+++ flask-server/service/Logging_Service.py
@@ -4,11 +4,6 @@ class Logging_Service(object):
 
         # Instantiates a client
         logging_client = logging.Client()
-
-        # The name of the log to write to
-        # log_name = "application-log"
-        # Selects the log to write to
-        # self.logger = logging_client.logger(log_name)
         logging_client.setup_logging()
 
     def info(self, text: str) -> None:
