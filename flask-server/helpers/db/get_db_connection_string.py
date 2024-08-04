7bd9e288d273412af3e06e62d701fd6cfd88112c
pvd232 <50126818+pvd232@users.noreply.github.com>
2024-08-04 16:03:27 -0400

    db

--- flask-server/helpers/db/get_db_connection_string.py
+++ flask-server/helpers/db/get_db_connection_string.py
@@ -1,7 +1,5 @@
-def get_db_connection_string(
-    username: str, password: str, db_name: str, db_server
-) -> str:
+def get_db_connection_string(username: str, password: str, db_name: str) -> str:
     c_string_beginning = "postgresql://"
-    c_string_end = f"@{db_server}/{db_name}"
+    c_string_end = f"@localhost:5432/{db_name}"
     c_string = f"{c_string_beginning}{username}:{password}{c_string_end}"
     return c_string
--- flask-server/models.py
+++ flask-server/models.py
@@ -67,10 +67,7 @@ password = os.getenv("DB_PASSWORD") or GCP_Secret_Manager_Service().get_secret(
 print("username", username)
 print("password", password)
 connection_string = os.getenv("DB_STRING") or get_db_connection_string(
-    username=username,
-    password=password,
-    db_name=os.getenv("DB_NAME"),
-    db_server=os.getenv("DB_SERVER"),
+    username=username, password=password, db_name="chera-prod-db"
 )
 print("connection_string", connection_string)
 
